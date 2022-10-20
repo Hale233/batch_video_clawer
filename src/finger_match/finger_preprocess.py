3b6157afea6f8d21a97110a89d9688263b99b2d1
Hale233 <1013581336@qq.com>
2022-10-20 20:35:54 +0800

    add slide win fun

--- src/finger_match/finger_preprocess.py
+++ src/finger_match/finger_preprocess.py
@@ -1,11 +1,21 @@
 from re import X
 import matplotlib.pyplot as plt
 import numpy as np
-class Chunk():
-    def __init__(self,finger_list,labels,file_path='') -> None:
+
+#记录流信息，分别是：指纹序列、五元组序列、视频URL、mitm文件路径(用于验证)
+class Video_flow():
+    def __init__(self,finger_list,tuple_list,video_url='',mitm_path='') -> None:
         self.finger_list=finger_list
-        self.labels=labels
-        self.file_path=file_path
+        self.tuple_list=tuple_list
+        self.video_url=video_url
+        self.mitm_path=mitm_path
+
+#记录原始流、ground truth流、预测流之间的关系
+class O_g_p_relation():
+    def __init__(self,original_stream=None,ground_truth_stream=None,pred_stream=None) -> None:
+        self.original_stream=original_stream
+        self.ground_truth_stream=ground_truth_stream
+        self.pred_stream=pred_stream
 
 class data_Process():
     def __init__(self,online_file,offline_file,audio_thd=600000) -> None:
@@ -14,11 +24,14 @@ class data_Process():
         self.audio_thd=audio_thd
         self.offline_chunk_list=[]
         self.online_chunk_list=[]
+        self.o_g_p_relation_list=[]
         self.get_offline_finger()
         self.get_online_finger()
-        self.small_chunk_clean(self.online_chunk_list)
-        self.small_chunk_clean(self.offline_chunk_list)
+        self.small_chunk_clean(self.online_chunk_list,audio_thd+10000)
+        self.small_chunk_clean(self.offline_chunk_list,audio_thd)
+        self.stream_label_match()
 
+    #获取离线指纹
     def get_offline_finger(self):
         offline_file=open(self.offline_file,mode='r',encoding='utf-8')
         offline_data=offline_file.read()
@@ -27,15 +40,18 @@ class data_Process():
             if lines=='':
                 continue
             vals=lines.split(',')
-            file_path=vals[0]#视频文件源路径
+            video_url=vals[3]#视频URL
             stream_type=vals[2]#video or audio
-            finger=vals[3].split('/')[1:]#指纹
-            tuples=vals[4].split('/')[1:]#五元组
+            finger=vals[4].split('/')[1:]#指纹
+            tuples=vals[5].split('/')[1:]#五元组
+            mitm_path=vals[0]#mitm_path路径
             if stream_type=='video':
-                self.offline_chunk_list.append(Chunk(list(map(int,finger)),tuples,file_path))
+                video_flow=Video_flow(list(map(int,finger)),tuples,video_url,mitm_path)
+                self.offline_chunk_list.append(video_flow)
             if stream_type=='audio':
                 pass
-        
+    
+    #获取在线指纹
     def get_online_finger(self):
         online_file=open(self.online_file,mode='r',encoding='utf-8')
         online_data=online_file.read()
@@ -46,16 +62,36 @@ class data_Process():
             vals=lines.split(',')
             finger=vals[1].split('/')[:-1]#指纹
             tuples=[vals[0]]#五元组
-            self.online_chunk_list.append(Chunk(list(map(int,finger)),tuples))
+            video_flow=Video_flow(list(map(int,finger)),tuples)
+            self.online_chunk_list.append(video_flow)
     
     #过滤掉指纹序列中小于阈值的块
-    def small_chunk_clean(self,chunk_list):
+    def small_chunk_clean(self,chunk_list,audio_thd):
         for chunk in chunk_list:
             clean_finger=[]
             for val in chunk.finger_list:
-                if val>self.audio_thd:
+                if val>audio_thd:
                     clean_finger.append(val)
             chunk.finger_list=clean_finger
+
+    #获取待匹配流的真实标签
+    def stream_label_match(self):
+        j=0
+        for online_chunk in self.online_chunk_list:
+            tmp=[online_chunk]
+            for offline_chunk in self.offline_chunk_list:
+                for offline_tuples in offline_chunk.tuple_list:
+                    if online_chunk.tuple_list[0]==offline_tuples:
+                        o_g_p_relation=O_g_p_relation(original_stream=online_chunk,ground_truth_stream=offline_chunk)
+                        tmp.append(offline_chunk)#用于debug分析
+            #验证是否出现在线流五元组对应多个目标流五元组的情况,仅记录一对一的情况
+            if len(tmp)==2:
+                self.o_g_p_relation_list.append(o_g_p_relation)
+                #统计仅用一条流传输视频的数量
+                #if len(o_g_p_relation.ground_truth_stream.tuple_list)==1:
+                #    j +=1
+        #print (len(self.offline_chunk_list))
+        #print (j)
 
     #分析音视频块的大小分布
     def chunk_plt(self,video_fingers,audio_fingers):
@@ -85,4 +121,4 @@ class data_Process():
         plt.plot(X_a,audio_fingers)
         plt.show()
 
-#finger_data=data_Process('/Users/hale/PycharmProjects/batch_video_clawer/data/res/online_encrypted_finger.csv','/Users/hale/PycharmProjects/batch_video_clawer/data/res/finger_store.csv')
+#finger_data=data_Process('/Users/hale/PycharmProjects/batch_video_clawer/data/result/online_encrypted_finger.csv','/Users/hale/PycharmProjects/batch_video_clawer/data/result/finger_store.csv')
--- src/finger_match/match_alg.py
+++ src/finger_match/match_alg.py
@@ -1,5 +1,6 @@
 from nis import match
-from finger_preprocess import data_Process,Chunk
+from symbol import small_stmt
+from finger_preprocess import data_Process,Video_flow,O_g_p_relation
 
 class Match_alg():
     def __init__(self,online_file,offline_file) -> None:
@@ -8,42 +9,59 @@ class Match_alg():
         #获取指纹数据
         finger_data=data_Process(online_file,offline_file)
         self.offline_chunk_list=finger_data.offline_chunk_list
-        self.online_chunk_list=finger_data.offline_chunk_list
+        self.online_chunk_list=finger_data.online_chunk_list
+        self.o_g_p_relation_list=finger_data.o_g_p_relation_list
     
     #滑动窗口匹配
     def slide_wind(self,wind_size):
-        no_match_count=0#长度太短而没有参与匹配的指纹数量
-        res_chunk=[]
-        for online_chunk in self.online_chunk_list:
-            if len(online_chunk.finger_list)<wind_size:
-                no_match_count +=1
+        index=-1
+        small_count=0
+        for o_g_p_relation in self.o_g_p_relation_list:
+            index +=1
+            # 仅统计用一条流传输的视频
+            #if len(o_g_p_relation.ground_truth_stream.tuple_list)!=1:
+            #    continue
+            if len(o_g_p_relation.original_stream.finger_list)<wind_size:
+                small_count +=1
                 continue
             min_dis=99999999999
             min_chunk=None
+            online_chunk=o_g_p_relation.original_stream
             for offline_chunk in self.offline_chunk_list:
-                if len(offline_chunk.finger_list)<wind_size:
+                if len(offline_chunk.finger_list)<wind_size:# or len(offline_chunk.finger_list)<len(online_chunk.finger_list):
                     continue
-                for i in range(0,len(offline_chunk.finger_list)-len(online_chunk.finger_list)):
+                for i in range(0,(len(offline_chunk.finger_list)-wind_size)):
+                #for i in range(0,(len(offline_chunk.finger_list)-len(online_chunk.finger_list)+1)):
                     cur_dis=0
                     for j in range(0,wind_size):
+                        #在线一定是要大于离线指纹库的
+                        if online_chunk.finger_list[j]<offline_chunk.finger_list[j+i]:
+                            cur_dis=99999999999
+                            break
                         cur_dis +=abs(online_chunk.finger_list[j]-offline_chunk.finger_list[j+i])
                     if cur_dis<min_dis:
                         min_dis=cur_dis
                         min_chunk=offline_chunk
             if min_chunk == None:
-                print("error")
+                #print("error")
                 continue
-            res_chunk.append([online_chunk,min_chunk])
-        
-        match_sucess_count=0
-        for match_chunk in res_chunk:
-            for match_off_chunk in match_chunk[1].labels:
-                if str(match_chunk[0].labels[0]).strip()==str(match_off_chunk).strip():
-                    print (str(match_chunk[0].labels[0])+' '+str(match_off_chunk))
-                    match_sucess_count +=1
-        print (match_sucess_count)
+            self.o_g_p_relation_list[index].pred_stream=min_chunk
+        print(small_count)
+    
+    #计算预测结果指标
+    def pred_performance(self):
+        true_count=0
+        all_count=0
+        for o_g_p_relation in self.o_g_p_relation_list:
+            if o_g_p_relation.pred_stream==None:
+                continue
+            all_count +=1
+            if o_g_p_relation.ground_truth_stream.video_url == o_g_p_relation.pred_stream.video_url:
+                true_count +=1
+        print ('all count {}; true count {}; acc {}'.format(all_count,true_count,true_count/all_count))
 
-match_alg=Match_alg('/Users/hale/PycharmProjects/batch_video_clawer/data/res/online_encrypted_finger.csv','/Users/hale/PycharmProjects/batch_video_clawer/data/res/finger_store.csv')
-match_alg.slide_wind(20)
+match_alg=Match_alg('/Users/hale/PycharmProjects/batch_video_clawer/data/result/online_encrypted_finger.csv','/Users/hale/PycharmProjects/batch_video_clawer/data/result/finger_store.csv')
+match_alg.slide_wind(10)
+match_alg.pred_performance()
                 
 
